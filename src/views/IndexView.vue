1d942828264d46756c51a41453aa614622ca0cb0
加糖舍曲林 <2451305814@qq.com>
2024-10-08 22:15:39 +0800

    修改了首页

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,65 +1,65 @@
 <template>
     <div class="w-full flex flex-col items-center justify-center mt-16">
 
-
-
         <!-- 左侧介绍文字+右侧图片的容器，使用flex布局 -->
-        <div class="w-full xl:w-5/6 flex flex-col lg:flex-row items-center">
+        <div class="w-5/6 ">
+            <div class="flex flex-col lg:flex-row items-center">
+                <!-- 左侧文字部分，如果lg尺寸，将order修改为1-->
+                <div class="flex-1 order-2 lg:order-1">
 
-            <!-- 左侧文字部分，如果lg尺寸，将order修改为1-->
-            <div class="flex-1 order-2 lg:order-1 lg:pl-12">
+                    <!-- 标题文字 -->
+                    <div
+                        class="mx-2 text-3xl text-center flex flex-col items-center md:text-4xl lg:items-start lg:text-5xl font-bold text-green-900">
+                        NEU百科指南
+                    </div>
 
-                <!-- 标题文字 -->
-                <div
-                    class="text-3xl text-center flex flex-col items-center md:text-4xl lg:items-start lg:text-5xl font-bold text-green-900">
-                    NEU百科指南
+                    <!-- 标题下面的次级标题 -->
+                    <div class="mx-2 text-3xl text-center flex flex-col items-center md:text-4xl lg:items-start 
+lg:text-start lg:text-5xl lg:leading-tight font-bold text-gray-900">
+                        为NEUer们提供的简单、快捷的校园信息查询
+                    </div>
+
+                    <!-- 交流QQ群号码 -->
+                    <div class="mx-2 text-xl text-center flex flex-col items-center lg:items-start  text-gray-500 mt-2">
+                        交流QQ群：2152012727
+                    </div>
+
+                    <!-- 开始阅读按钮 -->
+                    <div class="mx-2 flex justify-center  lg:justify-start mt-4">
+                        <n-button strong type="primary" round @click="routerTo('/read')">
+                            开始阅读
+                        </n-button>
+                    </div>
+
                 </div>
 
-                <!-- 标题下面的次级标题 -->
-                <div class="mx-8 text-3xl text-center flex flex-col items-center md:text-4xl lg:items-start 
-            lg:text-start lg:text-5xl lg:leading-tight font-bold text-gray-900">
-                    为NEUer们提供的简单、快捷的校园信息查询
+                <!-- 右侧的图片 -->
+                <div class="flex-1 order-1 lg:order-2 ">
+                    <n-image class="w-64 lg:w-full flex justify-center" width="400" src="/index_image.png" />
                 </div>
-
-                <!-- 交流QQ群号码 -->
-                <div class="text-xl text-center flex flex-col items-center lg:items-start  text-gray-500 mt-2">
-                    交流QQ群：2152012727
-                </div>
-
-                <!-- 开始阅读按钮 -->
-                <div class="flex justify-center  lg:justify-start mt-4">
-                    <n-button strong type="primary" round @click="routerTo('/read')">
-                        开始阅读
-                    </n-button>
-                </div>
-
             </div>
+            <div class="w-full flex flex-col lg:flex-row justify-around align-bottom">
+                <div class="flex-1 w-full lg:w-1/3 p-8 m-2 flex flex-col rounded-lg bg-gray-100"
+                    v-for="i in config.description_list" :key="i.title">
+                    <!-- 标题的图标 -->
+                    <div class="m-4">
+                        <n-icon size="30">
+                            <component :is="i.icon">
+                            </component>
+                        </n-icon>
 
-            <!-- 右侧的图片 -->
-            <div class="flex-1 order-1 lg:order-2 ">
-                <n-image class="w-64 lg:w-full flex justify-center" width="400" src="/index_image.png" />
+                    </div>
+                    <!-- 标题文字 -->
+                    <div class="text-xl  font-bold">
+                        {{ i.title }}
+                    </div>
+                    <div class="text-lg mt-2 text-gray-600">
+                        {{ i.content }}
+                    </div>
+                </div>
             </div>
         </div>
-        <div class=" px-2 md:px-12 w-5/6 box-border flex flex-col lg:flex-row justify-around align-bottom">
-            <div class="flex-1 w-full lg:w-1/3 p-8 m-2 flex flex-col rounded-lg bg-gray-100"
-                v-for="i in config.description_list" :key="i.title">
-                <!-- 标题的图标 -->
-                <div class="m-4">
-                    <n-icon size="30">
-                        <component :is="i.icon">
-                        </component>
-                    </n-icon>
 
-                </div>
-                <!-- 标题文字 -->
-                <div class="text-xl  font-bold">
-                    {{ i.title }}
-                </div>
-                <div class="text-lg mt-2 text-gray-600">
-                    {{ i.content }}
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
