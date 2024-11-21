5235aa91846f3e87d4f54849b849715a695df049
sugrsertraline <sugrsertraline@gmail.com>
2024-11-21 17:09:16 +0800

    完善了社团的介绍功能，使用弹出展示群聊二维码

--- src/components/NWClubIntro/NWClubIntroComponent.vue
+++ src/components/NWClubIntro/NWClubIntroComponent.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="bg-gray-50 p-4 sm:p-6 md:p-8 rounded-lg m-8">
-    <n-button type="primary" @click="showPreview"
-      class="mb-4 px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-md transition-colors duration-300">
-      Show Image
-    </n-button>
+
 
     <n-modal v-model:show="isPreviewVisible" :mask-closable="true" preset="card"
       style="width: auto; max-width: 90vw; max-height: 90vh;">
-      <img :src="imageUrl" alt="Preview Image" class="max-w-full max-h-[80vh] object-contain" @click.stop />
+      <img :src="ClubProps.imageUrl" alt="Preview Image" class="max-w-full max-h-[80vh] object-contain" @click.stop />
     </n-modal>
-    <div class="flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
-      <n-avatar :src="ClubProps.logo" :alt="ClubProps.name" :size="100" class="flex-shrink-0" />
+    <div
+      class="flex flex-col sm:flex-row items-center justify-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
+      <div class="flex flex-col justify-center items-center">
+        <n-avatar :src="ClubProps.logo" :alt="ClubProps.name" :size="100" class="flex-shrink-0" />
+
+        <n-button type="primary" @click="showPreview" class="mt-2 rounded-md transition-colors duration-300">
+          二维码
+          <template #icon>
+            <n-icon>
+              <QrCode />
+            </n-icon>
+          </template>
+        </n-button>
+      </div>
       <div class="flex-grow">
         <h2 class="text-2xl sm:text-3xl font-bold text-gray-800 mb-2 text-center sm:text-left">
           {{ ClubProps.name }}
         </h2>
+
         <p class="text-gray-600 mb-2 text-center sm:text-left">
           {{ ClubProps.description }}
         </p>
@@ -31,7 +41,8 @@
 
 <script setup lang="ts">
 import { NAvatar, NIcon } from 'naive-ui'
-import { Qq } from '@vicons/fa'
+import { Qq } from '@vicons/fa';
+import { QrCode } from '@vicons/ionicons5'
 import type { ClubProps } from '@/types/interface';
 import { NButton, NModal } from 'naive-ui'
 import { ref } from 'vue';
@@ -42,7 +53,6 @@ const props = defineProps<{
 }>()
 
 
-const imageUrl = ref('https://picsum.photos/1200/800')
 const isPreviewVisible = ref(false)
 
 const showPreview = () => {
