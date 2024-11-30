95bbdf21edf2672bf4abbb42fc3787c451d40519
加糖舍曲林 <2451305814@qq.com>
2024-11-30 17:30:25 +0800

    修改了tips的md语法

--- src/components/NWTips/NWTipsComponent.vue
+++ src/components/NWTips/NWTipsComponent.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="my-2">
       <n-alert :title="title" :type="case">
-      {{ data }}
+      {{ html_string }}
     </n-alert>
     </div>
 </template>
 <script setup lang="ts">
-const props = defineProps(['title','case','data'])
+import { stringToHtml } from '@/utils/utils';
+import { computed } from 'vue';
+
+const props = defineProps(['title','case','data']);
+const html_string = computed(() =>{
+    if(props.data!=undefined){
+        return stringToHtml(props.data);
+    }
+});
 </script>
